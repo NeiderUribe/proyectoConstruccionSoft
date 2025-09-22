f440d25cfeb3d8703ea58c813276676bdb2d80fd
Neider Uribe V <neider.uribe@correo.tdea.edu.co>
2025-09-22 01:33:15 -0500

    Actualice usersModel

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -2,32 +2,40 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database/db');
 
-const User = sequelize.define('Users', {
+const User = sequelize.define('User', {
     id:{
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
     name:{
-        type: DataTypes.STRING
+        type: DataTypes.STRING(50),
+        allowNull: false
     },
     email:{
-        type: DataTypes.STRING
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true
     },
     phone:{
-        type: DataTypes.STRING
+        type: DataTypes.STRING(10),
+        allowNull: false
     },
     password:{
-        type: DataTypes.STRING
+        type: DataTypes.STRING(50),
+        allowNull: false
     },
     address:{
-        type: DataTypes.STRING
+        type: DataTypes.STRING(150),
+        allowNull: false
     },
     neighborhood:{
-        type: DataTypes.STRING
+        type: DataTypes.STRING(100),
+        allowNull: false
     },
     is_active:{
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
     }
 });
 
